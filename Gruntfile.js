808a6575179ec4d9f8e432577020a3c333562f9f
Lauren Hamel <lauren.hamel@emory.edu>
2017-11-07 17:23:34 -0500

    Preliminary setup

---
+++ Gruntfile.js
@@ -0,0 +1,270 @@
+module.exports = function(grunt){
+  
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    pkgconfig: grunt.file.readJSON('package-config.json'),
+    watch: {
+      options: { livereload: true },
+      startup: {
+        files: [],
+        tasks: ['startup'],
+        options: { atBegin: true },
+      },
+      sass: {
+        files: ['src/scss/**/*.scss'],
+        tasks: ['sass:dev']
+      },
+      js: {
+        files: ['src/js/**/*.js'],
+        tasks: ['jshint:js', 'copy:js']
+      },
+      html: {
+        files: ['src/**/*.html'],
+        tasks: ['includes', 'replace:dev']
+      },
+      config: {
+        files: ['Gruntfile.js', 'package.json', 'composer.json'],
+        tasks: ['jshint:config', 'startup'],
+        options: { reload: true }
+      },
+      blog: {
+        files: ['posts/**/*', 'drafts/**/*'],
+        tasks: ['includes', 'replace:dev']
+      }
+    },
+    sass: {
+      dev: {
+        options: { style: 'expanded', noCache: true, update: true, sourcemap: 'none' },
+        files: [
+          { expand: true, cwd: 'src/scss/', src: ['*.scss'], dest: 'dist/css/', ext: '.css' }
+        ]
+      },
+      dist: {
+        options: { style: 'compressed', noCache: true, sourcemap: 'none' },
+        files: [
+          { expand: true, cwd: 'src/scss/', src: ['*.scss'], dest: 'dist/css/', ext: '.css' }
+        ]
+      }
+    },
+    postcss: {
+      options: { 
+        processors: [ require('autoprefixer')({ browsers: 'last 2 versions' }) ]
+      },
+      files: [
+        { expand: true, cwd: 'dist/css', src: ['**/*.css'], dest: '.', }
+      ]
+    },
+    cssmin: {
+      files: [
+        { 
+          expand: true, 
+          cwd: 'dist/css', 
+          src: ['**/*.css', '!**/*.min.css'], 
+          dest: '.',
+          ext: '.min.css'
+        }
+      ]
+    },
+    jshint: {
+      config: ['Gruntfile.js'],
+      js: ['src/js/**/*.js']
+    },
+    uglify: {
+      files: [
+        { expand: true, cwd: 'src/js/', src: ['**/*.js'], dest: 'dist/js' }
+      ]
+    },
+    replace: {
+      dev: {
+        options: {
+          patterns: [
+            { 
+              match: 'css', 
+              replacement: function(){
+                
+                var css = [], regex = /(\.min)?\.css/g, ext = '.css';
+          
+                grunt.config.get('pkgconfig').stylesheets.forEach(function(s){
+                  
+                  css.push(
+                    '<link rel="stylesheet" href="css/'+s.replace(regex, '')+ext+'">'
+                  );
+                  
+                });
+                
+                return css.join("\n  ");
+                
+              }
+            },
+            { 
+              match: 'js', 
+              replacement: function(){
+                
+                var js = [], regex = /(\.min)?\.js/g, ext = '.js';
+                
+                grunt.config.get('pkgconfig').scripts.forEach(function(s){
+                  
+                  js.push('<script src="js/'+s.replace(regex, '')+ext+'"></script>');
+                  
+                });
+                
+                return js.join("\n  ");
+                
+              }
+            },
+            { 
+              match: 'dependencies', 
+              replacement: function(){
+                
+                var dep = [], regex = /(\.min)?\.js/g, ext = '.min.js';
+                
+                Object.keys(grunt.config.get('pkg').dependencies).forEach(function(d){
+                  
+                  dep.push('<script src="js/dependencies/'+d.replace(regex, '')+ext+'"></script>');
+                  
+                });
+                
+                return dep.join("\n  ");
+                
+              }
+            },
+            {
+              match: 'livereload',
+              replacement: '<script src="//localhost:35729/livereload.js"></script>'
+            }
+          ],
+        },
+        files: [
+          {expand: true, cwd: 'src/', src: ['*.html'], dest: 'dist/'}
+        ]
+      },
+      dist: {
+        options: {
+          patterns: [
+            { 
+              match: 'css', 
+              replacement: function(){
+                
+                var css = [], regex = /(\.min)?\.css/g, ext = '.min.css';
+                
+                grunt.config.get('pkgconfig').stylesheets.forEach(function(s){
+                  
+                  css.push(
+                    '<link rel="stylesheet" href="css/'+s.replace(regex, '')+ext+'">'
+                  );
+                  
+                });
+                
+                return css.join("\n  ");
+                
+              }
+            },
+            { 
+              match: 'js', 
+              replacement: function(){
+                
+                var js = [], regex = /(\.min)?\.js/g, ext = '.min.js';
+                
+                grunt.config.get('pkgconfig').scripts.forEach(function(s){
+                  
+                  js.push('<script src="js/'+s.replace(regex, '')+ext+'"></script>');
+                  
+                });
+                
+                return js.join("\n  ");
+                
+              }
+            },
+            { 
+              match: 'dependencies', 
+              replacement: function(){
+                
+                var dep = [], regex = /(\.min)?\.js/g, ext = '.min.js';
+                
+                Object.keys(grunt.config.get('pkg').dependencies).forEach(function(d){
+                  
+                  dep.push('<script src="js/dependencies/'+d.replace(regex, '')+ext+'"></script>');
+                  
+                });
+                
+                return dep.join("\n  ");
+                
+              }
+            },
+            {
+              match: 'livereload',
+              replacement: ''
+            }
+          ],
+        },
+        files: [
+          {expand: true, cwd: 'src/', src: ['*.html'], dest: 'dist/'}
+        ]
+      }
+    },
+    copydeps: {
+      dependencies: {
+        pkg: 'package.json',
+        dest: 'dist/js/dependencies/'
+      }
+    },
+    includes: {
+      html: {
+        options: { includePath: 'src/includes/' },
+        files: [
+          {expand: true, cwd: 'src/', src: ['*.html'], dest: 'dist/' }
+        ]
+      }
+    },
+    copy: {
+      js: {
+        files: [
+          { expand: true, cwd: 'src/js', src: ['*.js'], dest: 'dist/js' }
+        ]
+      },
+      assets: {
+        files: [
+          { expand: true, cwd: 'src/images/', src: ['**'], dest: 'dist/images/' },
+          { expand: true, cwd: 'src/fonts/', src: ['**'], dest: 'dist/fonts/' }
+        ]
+      }
+    },
+    clean: ['dist/']
+  });
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-copy-deps');
+  grunt.loadNpmTasks('grunt-replace');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-includes');
+  
+  grunt.registerTask('default', ['dev']);
+  grunt.registerTask('startup', [
+    'copydeps',
+    'sass:dev',
+    'jshint',
+    'copy:js',
+    'copy:assets',
+    'includes',
+    'replace:dev'
+  ]);
+  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask('dist', [
+    'clean',
+    'copydeps',
+    'sass:dist',
+    'postcss',
+    'cssmin',
+    'uglify',
+    'copy:assets',
+    'includes',
+    'replace:dist'
+  ]);
+  
+};
---
+++ posts/test.md
@@ -0,0 +1,9 @@
+---
+title: This is My First Official Blog Post
+description: This is a short description about this blog post.
+date: 
+date-created: 11-06-2017 12:00 PM
+date-modified: 11-07-2017 1:00 AM
+---
+
+This is some markdown text.
