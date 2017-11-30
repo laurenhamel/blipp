db659e7ab3f8835f52dd6d021ce3f834a9105f3a
Lauren Hamel <lauren.hamel@emory.edu>
2017-11-30 10:52:33 -0500

    Breaks out meta class and traits

---
+++ php/classes/Meta.class.php
@@ -0,0 +1,159 @@
+<?php
+
+class Meta {
+  
+  private $regex = [
+    'meta'    => '/---(?:\S|\s)*---/',
+    'break'   => '/\r*\n+|\r+/',
+    'bound'   => '/---/',
+    'data'    => '/:/'
+  ];
+  private $raw = [];
+  private $meta = [];
+  
+  public function __construct( $data ) {
+    $this->meta = $this->parseMeta( $data );
+  }
+  
+  public function __get( $value ) {
+    return $this->meta[$value];
+  }
+  
+  public function __set( $keys, $value ) {
+    
+    $keys = explode('.', $keys);
+    $meta = &$this->meta;
+    
+    foreach( $keys as $key ) {
+      
+      if( !array_key_exists($key, $meta) ) return false;
+      
+      $meta = &$meta[$key];
+      
+    }
+    
+    $meta = $value;
+    
+    return true;
+    
+  }
+  
+  public function getMeta( $raw = false ) {
+    return $raw === true ? $this->raw : $this->meta;
+  }
+  
+  private function formatMeta( $string ) {
+    
+    // Handle strings only
+    if( gettype($string) !== 'string' ) return $string;
+
+    // Array types
+    if( preg_match('/^\[(.+?)\]$/', $string) ) {
+
+      $string = array_map('trim', explode(',', preg_replace('/\[|\]/', '', $string)));
+
+      foreach( $string as $key => &$value ) {
+
+        $string[$key] = $this->formatMeta( $value );
+
+      }
+
+    }
+    
+    // Object types
+    elseif( preg_match('/^\{ *(.+?) *\}$/', $string) ) {
+
+      $string = array_map(function($meta){
+        return array_map('trim', preg_split('/:/', $meta, 2));
+      }, array_map('trim', preg_split(
+        '/,(?=[^\]]*(?:[\[]|$))/', preg_replace('/\{|\}/', '', $string)
+      )));
+
+      foreach( $string as $index => $array ) {
+        
+        $string[$array[0]] = $this->formatMeta( $array[1] );
+        
+        unset($string[$index]);
+        
+      }
+      
+    }
+    
+    // Date types
+    elseif( (bool) strtotime($string) ) {
+      
+      $string = new Moment($string);
+      
+    }
+    
+    // Number types
+    elseif( is_numeric($string) ) {
+      
+      $string = (float) $string;
+      
+    }
+    
+    // Boolean types
+    elseif( $string == 'true' or $string == 'false' ) {
+      
+      $string = $string == 'true' ? true : false;
+      
+    }
+ 
+    return $string;
+    
+  }
+  
+  private function parseMeta( $data ) {
+    
+    // Get the regular expressions.
+    $regex = $this->regex;
+    
+    // Extract
+    preg_match($regex['meta'], $data, $match);
+    
+    // Save the raw data.
+    $this->raw = $raw = $match[0];
+    
+    // Remove data boundaries, and create a basic array from the data.
+    $raw = preg_split($regex['break'], trim(preg_replace($regex['bound'], '', $raw)));
+    
+    // Interpret the raw data.
+    $raw = array_map(function($pair) use ($regex){
+      
+      $array = array_map('trim', preg_split($regex['data'], $pair, 2));
+
+      return $array;
+      
+    }, array_filter($raw, function($item){
+      
+      return isset($item);
+      
+    }));
+    
+    // Format the raw data.
+    foreach( $raw as &$meta ) { 
+      
+      $meta[1] = $this->formatMeta( $meta[1] );
+  
+    }
+    
+    // Reduce the raw data.
+    for( $i = count($raw) - 1; $i > -1; $i-- ) { 
+      
+      $meta = $raw[$i];
+      
+      $raw[$meta[0]] = $meta[1];
+      
+      unset($raw[$i]);
+      
+    }
+
+    // Return the real meta data.
+    return array_merge( $raw );
+    
+  }
+  
+}
+
+?>
--- src/php/classes/Markdown.class.php
+++ src/php/classes/Markdown.class.php
@@ -1,22 +1,64 @@
 <?php
 
-class Markdown {
+trait EXTENSIBLE {
   
-  public $path;
-  public $filename;
-  public $contents;
+  public $extensions = [];
+  
+  private function registerExtensions() {
+    
+    $path = API_PATH.'/extensions/';
+    
+    $extensions = array_values(array_filter(scandir($path), function( $item ){
+      return !preg_match('/^\.+$/', $item);
+    }));
+    
+    foreach($extensions as $extension){
+      
+      $name = str_replace('.php', '', $extension);
+      
+      $this->extensions[$name] = include $path.$extension;
+      
+    }
+    
+  }
+  
+  private function applyExtensions( $string ) {
+    
+    foreach( $this->extensions as $type => $function ){
+      
+      $string = $function( $string );
+     
+    }
+   
+    return $string;
+    
+  }
+  
+}
+
+trait CONFIGURABLE {
+  
   public $config = [
     'date' => [
       'long'        => 'F j, Y',
       'short'       => 'm.d.Y',
     ]
   ];
+  
+}
+
+class Markdown {
+  
+  use CONFIGURABLE, EXTENSIBLE;
+  
+  public $path;
+  public $filename;
+  public $contents;
   public $id;
   public $meta = [];
   public $frontmatter;
   public $markdown;
   public $html;
-  public $extensions = [];
   
   function __construct( $path ) {
 
@@ -25,7 +67,7 @@ class Markdown {
     $this->id = filename_id( $this->filename );
     $this->contents = file_get_contents($this->path);
     $this->registerExtensions();
-    $this->parseFrontmatter();
+    $this->parseMeta();
     $this->parseMarkdown();
     
   }
@@ -83,171 +125,37 @@ class Markdown {
     
   }
   
-  private function formatFrontmatter( $string ) {
+  private function parseMeta(){
     
-    // Handle strings only
-    if( gettype($string) !== 'string' ) return $string;
-
-    // Array types
-    if( preg_match('/^\[(.+?)\]$/', $string) ) {
-
-      $string = array_map('trim', explode(',', preg_replace('/\[|\]/', '', $string)));
-
-      foreach( $string as $key => &$value ) {
-
-        $string[$key] = $this->formatFrontmatter( $value );
-
-      }
-
-    }
+    // Parse meta data.
+    $meta = new Meta( $this->contents );
     
-    // Object types
-    elseif( preg_match('/^\{ *(.+?) *\}$/', $string) ) {
-
-      $string = array_map(function($meta){
-        return array_map('trim', preg_split('/:/', $meta, 2));
-      }, array_map('trim', preg_split(
-        '/,(?=[^\]]*(?:[\[]|$))/', preg_replace('/\{|\}/', '', $string)
-      )));
-
-      foreach( $string as $index => $array ) {
-        
-        $string[$array[0]] = $this->formatFrontmatter( $array[1] );
-        
-        unset($string[$index]);
-        
-      }
-      
-    }
+    // Create mustache methods.
+    $methods = $this->setMustacheMethods( $meta );
     
-    // Date types
-    elseif( (bool) strtotime($string) ) {
-      
-      $string = new Moment($string);
-      
-    }
+    // Save meta data.
+    $this->frontmatter = $meta->getMeta(true);
+    $this->meta = array_merge($meta->getMeta(), $methods);
     
-    // Number types
-    elseif( is_numeric($string) ) {
-      
-      $string = (float) $string;
-      
-    }
-    
-    // Boolean types
-    elseif( $string == 'true' or $string == 'false' ) {
-      
-      $string = $string == 'true' ? true : false;
-      
-    }
- 
-    return $string;
-    
-  }
-  
-  private function registerExtensions() {
-    
-    $path = API_PATH.'/extensions/';
-    
-    $extensions = array_values(array_filter(scandir($path), function( $item ){
-      return !preg_match('/^\.+$/', $item);
-    }));
-    
-    foreach($extensions as $extension){
-      
-      $name = str_replace('.php', '', $extension);
-      
-      $this->extensions[$name] = include $path.$extension;
-      
-    }
-    
-  }
-  
-  private function applyExtensions( $string ) {
-    
-    foreach( $this->extensions as $type => $function ){
-      
-      $string = $function( $string );
-     
-    }
-   
-    return $string;
-    
-  }
-  
-  private function parseFrontmatter(){
-    
-    // Define some regular expressions.
-    $regex = [
-      'frontmatter' => '/---(?:\S|\s)*---/',
-      'linebreak'   => '/\r*\n+|\r+/',
-      'delimiter'   => '/---/',
-      'separator'   => '/:/'
-    ];
-    
-    // Extract
-    preg_match($regex['frontmatter'], $this->contents, $match);
-    
-    // Save
-    $this->frontmatter = $frontmatter = $match[0];
-    
-    // Interpret
-    $frontmatter = array_map(function($pair) use ($regex){
-      
-      $array = array_map('trim', preg_split($regex['separator'], $pair, 2));
-
-      return $array;
-      
-    }, array_filter( preg_split(
-      
-        $regex['linebreak'], trim( preg_replace($regex['delimiter'], '', $frontmatter) )
-      
-      ), function($item){
-      
-      return isset($item);
-      
-    }));
-    
-    // Format
-    foreach( $frontmatter as &$meta ) { 
-      
-      $meta[1] = $this->formatFrontmatter( $meta[1] );
-  
-    }
-    
-    // Reduce
-    for( $i = count($frontmatter) - 1; $i > -1; $i-- ) { 
-      
-      $meta = $frontmatter[$i];
-      
-      $frontmatter[$meta[0]] = $meta[1];
-      
-      unset($frontmatter[$i]);
-      
-    }
-
-    // Save
-    $this->meta = array_merge(
-      $frontmatter, 
-      $this->setMustacheMethods( $frontmatter )
-    );
-    $this->slug = slug_id( $this->meta['title'] );
+    // Set the slug.
+    $this->slug = slug_id( $meta->title );
 
   }
   
   private function parseMarkdown(){
     
-    // Extract
-    $this->markdown = preg_replace('/---(?:\S|\s)*---/', '', $this->contents);
+    // Extract markdown.
+    $this->markdown = $markdown = preg_replace('/---(?:\S|\s)*---/', '', $this->contents);
     
-    // Parse
+    // Extract meta data.
+    $meta = $this->meta;
+    
+    // Create parsers.
     $Parsedown = new Parsedown();
     $Mustache = new Mustache();
-    $this->html = $Parsedown->text( 
-      $this->applyExtensions(
-        $Mustache->render($this->markdown, $this->meta) 
-      )
-    );
+    
+    // Build HTML.
+    $this->html = $Parsedown->text( $this->applyExtensions($Mustache->render($markdown, $meta)));
     
   }
   
